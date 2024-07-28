2579c3513fa5bb01ef3cd4df0b11957977f32174
Angel <91911550+rebolledoangel@users.noreply.github.com>
2024-07-27 22:33:45 -0400

    Update oopExample.php

--- oopExample.php
+++ oopExample.php
@@ -16,5 +16,52 @@ $myCar=new Car();
 $mycar->setColor('Red and Blue');
 echo 'The car color is: '. $mycar->getColor();
 
+//INHERITANCE
+class Animal{
+  protected $name;
+
+  public function __construct($name){
+    $this->name=$name;
+  }
+}
+
+class Dog extends Animal{
+  public function makesound(){
+    echo 'Bark';
+  }
+
+  public function dogName(){
+    echo $this->name . ' does not like '
+  }
+}
+echo '------INHERITANCE------';
+$mydog=new Dog('Princess');
+$mydog->makesound(). $mydog->dogName;
+
+//ABSTRACTION
+abstract class Animal(){
+  protected $name;
+
+  public function __contruct($name){
+    $this->name=$name;
+  }
+
+  abstract public fucntion makeSound();
+}
+
+class Dog extends Animal{
+  public function makeSound(){
+    return 'Bark';
+  }
+}
+
+class Cat extends Animal{
+  public function makeSound(){
+    return 'Meow';
+  }
+}
+$mydog=new Dog('Princess');
+echo 'My dog '.$mydog->name.' does not like '. $mydog->makesound();
+
 
 ?>
